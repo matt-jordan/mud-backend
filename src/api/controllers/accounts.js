8aa3668c39f4859deef9c0022859195f02d38187
Matt Jordan <mjordan@digium.com>
2021-12-31 09:57:09 -0800

    lib/errors: Add default HTTP errors to force JSON responses
    By default, Express will not send errors back as JSON. This can
    result in some reptetive boilerplate leaking around, as well as
    making it easy to use 'sendStatus' which will not produce the result
    we want. While it's a bit funky, this adds a set of Errors that can
    be created and passed to next(), which will percolate down to the
    default error handler. The default error handler in turn will properly
    send the HTTP error as JSON.

--- src/api/controllers/accounts.js
+++ src/api/controllers/accounts.js
@@ -1,15 +1,16 @@
 import { Router } from 'express';
 
 import AccountModel from '../../db/models/Account.js';
+import { BadRequestError, NotFoundError, ConflictError } from '../../lib/errors.js';
 
 const router = Router();
 
-router.get('/:accountId', async (req, res) => {
+router.get('/:accountId', async (req, res, next) => {
   const accountName = req.params.accountId;
 
   const account = await AccountModel.findOne({ accountName }).exec();
   if (!account) {
-    return res.status(404).send({ message: `Account '${accountName}' not found` });
+    return next(new NotFoundError(`Account '${accountName}' not found`));
   }
 
   return res.status(200).json(account.toObject());
@@ -23,24 +24,24 @@ router.get('/:accountId', async (req, res) => {
 //  - At least one special character
 const passwordRegex = /^(?=.*[\d])(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*])[\w!@#$%^&*]{8,32}$/;
 
-router.post('/:accountId', async (req, res) => {
+router.post('/:accountId', async (req, res, next) => {
   const accountName = req.params.accountId;
 
   const existingAccount = await AccountModel.findOne({ accountName }).exec();
   if (existingAccount) {
-    return res.status(409).send({ message: `Account '${accountName}' already exists` });
+    return next(new ConflictError(`Account '${accountName}' already exists`));
   }
 
   const { password, email } = req.body;
   if (!password) {
-    return res.status(400).send({ message: 'Accounts must have a password' });
+    return next(new BadRequestError('Accounts must have a password'));
   }
   if (!email) {
-    return res.status(400).send({ message: 'Accounts must have an email' });
+    return next(new BadRequestError('Accounts must have an email'));
   }
 
   if (!passwordRegex.test(password)) {
-    return res.status(400).send({ message: 'Account password failed complexity check' });
+    return next(new BadRequestError('Account password failed complexity check'));
   }
 
   const account = new AccountModel();
@@ -52,17 +53,17 @@ router.post('/:accountId', async (req, res) => {
   return res.status(201).json(account.toObject());
 });
 
-router.put('/:accountId', async (req, res) => {
+router.put('/:accountId', async (req, res, next) => {
   const accountName = req.params.accountId;
 
   const account = await AccountModel.findOne({ accountName }).exec();
   if (!account) {
-    return res.status(404).send({ message: `Account '${accountName}' not found` });
+    return next(new NotFoundError(`Account '${accountName}' not found`));
   }
 
   const { password, email } = req.body;
   if (password && !passwordRegex.test(password)) {
-    return res.status(400).send({ message: 'Account password failed complexity check' });
+    return next(new BadRequestError('Account password failed complexity check'));
   }
 
   account.password = password || account.password;
@@ -73,13 +74,13 @@ router.put('/:accountId', async (req, res) => {
   return res.status(200).json(account.toObject());
 });
 
-router.put('/:accountId/characters/:characterId', async (req, res) => {
+router.put('/:accountId/characters/:characterId', async (req, res, next) => {
   const accountName = req.params.accountId;
   const characterId = req.params.characterId;
 
   const account = await AccountModel.findOne({ accountName }).exec();
   if (!account) {
-    return res.status(404).send({ message: `Account '${accountName}' not found` });
+    return next(new NotFoundError(`Account '${accountName}' not found`));
   }
 
   if (!account.characterIds.includes(characterId)) {
@@ -90,13 +91,13 @@ router.put('/:accountId/characters/:characterId', async (req, res) => {
   return res.status(200).json(account.toObject());
 });
 
-router.delete('/:accountId/characters/:characterId', async (req, res) => {
+router.delete('/:accountId/characters/:characterId', async (req, res, next) => {
   const accountName = req.params.accountId;
   const characterId = req.params.characterId;
 
   const account = await AccountModel.findOne({ accountName }).exec();
   if (!account) {
-    return res.status(404).send({ message: `Account '${accountName}' not found` });
+    return next(new NotFoundError(`Account '${accountName}' not found`));
   }
 
   account.characterIds = account.characterIds.filter((id) => id !== characterId);
--- src/api/middleware/defaultErrorHandler.js
+++ src/api/middleware/defaultErrorHandler.js
@@ -1,9 +1,17 @@
 import log from '../../lib/log.js';
 
 const defaultErrorHandler = (err, req, res, next) => {
-  log.error({ err }, 'Unhandled API exception');
-  res.status(500).json({ error: err });
-  next();
+  if (!err) {
+    return next();
+  }
+
+  if (!err.statusCode) {
+    res.status(500);
+    log.error({ err }, 'Unhandled API exception');
+  } else {
+    res.status(err.statusCode);
+  }
+  res.json({ message: err.message });
 };
 
 export default defaultErrorHandler;
