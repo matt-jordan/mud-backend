db47e63025c48efdae1ff5cd181181df20d332df
Matt Jordan <mjordan@digium.com>
2022-01-22 08:40:02 -0800

    Look: Add the Look command
    This adds the Look command, mostly ported over from the failed
    attempt at making this game (haha... ugh.) This consists of two
    parts: A factory which will generate the action from the supplied
    tokens, and the action itself which takes the player character
    and manipulates them and their environment. The test covers
    looking at both the current room as well as those rooms that may
    or may not be adjacent.
    
    Note that eventually you can also look at objects and other things,
    which is comented out for now. We'll get there.

---
+++ src/game/commands/default/Look.js
@@ -0,0 +1,113 @@
+
+class LookAction {
+  constructor(options = {}) {
+    this.direction = options.direction;
+    this.object = options.object;
+  }
+
+  async execute(character) {
+    if (!character.room) {
+      character.sendImmediate('You are floating in a void');
+      return;
+    }
+    const room = character.room;
+
+    if (!this.direction && !this.object) {
+      character.sendImmediate(room.toText());
+      return;
+    }
+
+    if (this.direction) {
+      if (!(this.direction in room.exits)) {
+        character.sendImmediate('There is nothing in that direction.');
+        return;
+      }
+      const exit = room.exits[this.direction];
+      const destination = character.world.findRoomById(exit.destinationId);
+      if (!destination) {
+        character.sendImmediate('There is nothing in that direction.');
+        return;
+      }
+      character.sendImmediate(await destination.toShortText());
+      return;
+    }
+
+    /*
+    let foundObject;
+    let searchTerm;
+    let index = -1;
+    if (!this.object.includes('.')) {
+      searchTerm = this.object;
+    } else {
+      const tokens = this.object.split('.');
+      index = parseInt(tokens[0], 10);
+      searchTerm = tokens[1].trim();
+    }
+
+    const things = room.characters.concat(room.inanimates);
+    const objects = things.filter(i => i.name === searchTerm);
+    if (objects.length === 0) {
+      character.sendImmediate(`${this.object} is not here.`);
+    } else if (objects.length > 1) {
+      if (index < 0) {
+        character.sendImmediate(`Which ${this.object} do you want?`);
+      } else if (index > objects.length - 1) {
+        character.sendImmediate(`There are not ${index} ${searchTerm} here.`);
+      } else {
+        foundObject = objects[index];
+      }
+    } else {
+      foundObject = objects[0];
+    }
+
+    if (foundObject) {
+      character.sendImmediate(`${foundObject.toShortText()}`);
+    }
+    */
+  }
+}
+
+class LookFactory {
+  static get name() {
+    return 'Look';
+  }
+
+  constructor() {
+    this.options = [ [
+      'north',
+      'south',
+      'west',
+      'east',
+      'up',
+      'down',
+      'northwest',
+      'northeast',
+      'southwest',
+      'southeast',
+    ] ];
+  }
+
+  generate(tokens) {
+    if (tokens.length === 0) {
+      return new LookAction();
+    }
+    if (tokens.length === 1) {
+      const direction = this.options[0].find((option) => option === tokens[0]);
+      if (!direction) {
+        // TODO: Return an Error message action
+        return null;
+      }
+      return new LookAction({ direction });
+    } else if (tokens.length > 2 && tokens[0] === 'at') {
+      return new LookAction({ object: tokens.slice(1).join(' ') });
+    }
+    // TODO: return an Error message action
+    return null;
+  }
+
+}
+
+export {
+  LookAction,
+  LookFactory,
+};
---
+++ test/game/commands/default/testLook.js
@@ -0,0 +1,181 @@
+import mongoose from 'mongoose';
+import assert from 'power-assert';
+import EventEmitter from 'events';
+
+import { LookFactory, LookAction } from '../../../../src/game/commands/default/Look.js';
+
+import World from '../../../../src/game/world/world.js';
+import PlayerCharacter from '../../../../src/game/characters/playerCharacter.js';
+import CharacterModel from '../../../../src/db/models/Character.js';
+import AreaModel from '../../../../src/db/models/Area.js';
+import RoomModel from '../../../../src/db/models/Room.js';
+import getOpposingDirection from '../../../../src/lib/getOpposingDirection.js';
+
+class FakeClient extends EventEmitter {
+  constructor(msgCb) {
+    super();
+    this.receivedMessage = null;
+    this.closed = false;
+    this.msgCb = msgCb;
+  }
+
+  send(message) {
+    if (this.msgCb && !this.receivedMessage) {
+      this.msgCb(message);
+    }
+    this.receivedMessage = message;
+  }
+
+  close() {
+    this.closed = true;
+  }
+}
+
+describe('LookAction', () => {
+  [
+    'up',
+    'down',
+    'north',
+    'east',
+    'west',
+    'south',
+    'southeast',
+    'southwest',
+    'northeast',
+    'northwest'
+  ].forEach((direction) => {
+    describe(`when looking ${direction}`, () => {
+      let world;
+      let pc;
+
+      beforeEach(async () => {
+        const areaModel = new AreaModel();
+        areaModel.name = 'TestArea';
+
+        const roomModel1 = new RoomModel();
+        roomModel1.name = 'TestRoom1';
+        roomModel1.areaId = areaModel._id;
+
+        const roomModel2 = new RoomModel();
+        roomModel2.name = 'TestRoom2';
+        roomModel2.areaId = areaModel._id;
+        roomModel1.exits.push({
+          direction,
+          destinationId: roomModel2._id,
+        });
+        roomModel2.exits.push({
+          direction: getOpposingDirection(direction),
+          destinationId: roomModel1._id,
+        });
+        await roomModel1.save();
+        await roomModel2.save();
+
+        areaModel.roomIds.push(roomModel1._id);
+        areaModel.roomIds.push(roomModel2._id);
+        await areaModel.save();
+
+        const fakeTransport = new EventEmitter();
+        world = new World(fakeTransport);
+        await world.load();
+
+        const characterModel = new CharacterModel();
+        characterModel.name = 'TestCharacter';
+        characterModel.accountId = new mongoose.Types.ObjectId();
+        characterModel.description = 'A complete character';
+        characterModel.age = 30;
+        characterModel.gender = 'non-binary';
+        characterModel.roomId = roomModel1._id;
+        characterModel.classes.push({
+          type: 'fighter',
+          level: 1,
+          experience: 0,
+        });
+        characterModel.attributes = {
+          strength: { base: 18, },
+          dexterity: { base: 12, },
+          constitution: { base: 14, },
+          intelligence: { base: 12, },
+          wisdom: { base: 8, },
+          charisma: { base: 8, },
+          hitpoints: { base: 6, current: 6, },
+          manapoints: { base: 6, current: 6, },
+          energypoints: { base: 10, current: 10, },
+        };
+        await characterModel.save();
+
+        pc = new PlayerCharacter(characterModel, world);
+        await pc.load();
+      });
+
+      afterEach(async () => {
+        await CharacterModel.deleteMany();
+        await AreaModel.deleteMany();
+        await RoomModel.deleteMany();
+      });
+
+      it('states that there is nothing in the direction when there isnt', (done) => {
+        const otherDirection = getOpposingDirection(direction);
+        const action = new LookAction({ direction: otherDirection });
+        const transport = new FakeClient((msg) => {
+          assert(msg);
+          assert.match(msg, /TextMessage/);
+          assert.match(msg, /There is nothing in that direction/);
+          done();
+        });
+        pc.transport = transport;
+        action.execute(pc);
+      });
+
+      it('returns a brief description when there is an exit', (done) => {
+        const action = new LookAction({ direction });
+        const transport = new FakeClient((msg) => {
+          assert(msg);
+          assert.match(msg, /TextMessage/);
+          done();
+        });
+        pc.transport = transport;
+        action.execute(pc);
+      });
+    });
+  });
+});
+
+
+describe('LookFactory', () => {
+  describe('when generating an action', () => {
+
+    let factory;
+    beforeEach(() => {
+      factory = new LookFactory();
+    });
+
+    it('handles an empty token list', () => {
+      const result = factory.generate([]);
+      assert(result);
+    });
+
+    it('handles an action that is too long', () => {
+      const result = factory.generate(['up', 'now']);
+      assert(!result);
+    });
+
+    [
+      'up',
+      'down',
+      'north',
+      'east',
+      'west',
+      'south',
+      'southeast',
+      'southwest',
+      'northeast',
+      'northwest'
+    ].forEach((direction) => {
+      it(`handles ${direction}`, () => {
+        const result = factory.generate([ direction ]);
+        assert(result);
+        assert(result.direction === direction);
+      });
+    });
+  });
+});
