22d3fa3e4f4c6509c5dd020e6b1bf268ca564722
Matt Jordan <mjordan@digium.com>
2022-01-24 19:59:25 -0800

    db: Add Weapon model
    This adds a weapon model that should allow us to construct most
    weapons in the 5E D20 system.

---
+++ src/db/models/Weapon.js
@@ -0,0 +1,28 @@
+//------------------------------------------------------------------------------
+// MJMUD Backend
+// Copyright (C) 2022, Matt Jordan
+//
+// This program is free software, distributed under the terms of the
+// MIT License. See the LICENSE file at the top of the source tree.
+//------------------------------------------------------------------------------
+
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const weaponSchema = new Schema({
+  name: { type: String, required: true },
+  description: { type: String },
+  properties: [{ type: String, enum: ['ammunition', 'finesse', 'heavy', 'light', 'loading', 'range', 'reach', 'two-handed', 'versatile']}],
+  damageType: { type: String, required: true, enum: [ 'piercing', 'slashing', 'bludgeoning' ]},
+  weaponType: { type: String, required: true, enum: [ 'simple', 'martial', 'exotic' ]},
+  classRestriction: [{ type: String }],
+  levelRestriction: { type: Number, default: 0, },
+  weight: { type: Number, default: 1, required: true },
+  minDamage: { type: Number, default: 1 },
+  maxDamage: { type: Number, default: 1 },
+});
+
+const WeaponModel = mongoose.model('Weapon', weaponSchema);
+
+export default WeaponModel;
---
+++ test/db/models/testWeapon.js
@@ -0,0 +1,81 @@
+//------------------------------------------------------------------------------
+// MJMUD Backend
+// Copyright (C) 2022, Matt Jordan
+//
+// This program is free software, distributed under the terms of the
+// MIT License. See the LICENSE file at the top of the source tree.
+//------------------------------------------------------------------------------
+
+import assert from 'power-assert';
+
+import WeaponModel from '../../../src/db/models/Weapon.js';
+
+describe('WeaponModel', () => {
+  afterEach(async () => {
+    await WeaponModel.deleteMany();
+  });
+
+  describe('creating', () => {
+    it('rejects when there is no name', async () => {
+      const uut = new WeaponModel();
+      uut.description = 'A weapon';
+      uut.properties.push('heavy');
+      uut.properties.push('reach');
+      uut.damageType = 'slashing';
+      uut.weaponType = 'martial';
+      uut.classRestriction.push('fighter');
+      uut.levelRestriction = 5;
+      uut.weight = 3;
+      uut.minDamage = 2;
+      uut.maxDamage = 12;
+      await assert.rejects(uut.save());
+    });
+
+    it('reject when there is no damageType', async () => {
+      const uut = new WeaponModel();
+      uut.name = 'test';
+      uut.description = 'A weapon';
+      uut.properties.push('heavy');
+      uut.properties.push('reach');
+      uut.weaponType = 'martial';
+      uut.classRestriction.push('fighter');
+      uut.levelRestriction = 5;
+      uut.weight = 3;
+      uut.minDamage = 2;
+      uut.maxDamage = 12;
+      await assert.rejects(uut.save());
+    });
+
+    it('reject when there is no weaponType', async () => {
+      const uut = new WeaponModel();
+      uut.name = 'test';
+      uut.description = 'A weapon';
+      uut.properties.push('heavy');
+      uut.properties.push('reach');
+      uut.damageType = 'slashing';
+      uut.classRestriction.push('fighter');
+      uut.levelRestriction = 5;
+      uut.weight = 3;
+      uut.minDamage = 2;
+      uut.maxDamage = 12;
+      await assert.rejects(uut.save());
+    });
+
+    it('creates the weapon', async () => {
+      const uut = new WeaponModel();
+      uut.name = 'test';
+      uut.description = 'A weapon';
+      uut.properties.push('heavy');
+      uut.properties.push('reach');
+      uut.damageType = 'slashing';
+      uut.weaponType = 'martial';
+      uut.classRestriction.push('fighter');
+      uut.levelRestriction = 5;
+      uut.weight = 3;
+      uut.minDamage = 2;
+      uut.maxDamage = 12;
+      await uut.save();
+      assert(uut);
+    });
+  });
+});
