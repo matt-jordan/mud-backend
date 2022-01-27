7f7fc2c136bdc190e031ef5b7b29856aea4f7417
Matt Jordan <mjordan@digium.com>
2022-01-27 07:30:13 -0800

    GetItem: Add a command for picking up items
    This adds a command that can be used to pick items up from
    rooms or pulled from containers. It does not currently have the
    ability to separate out items with like names and is case
    sensitive, so we'll have to address that at some point.

---
+++ src/game/commands/default/GetItem.js
@@ -0,0 +1,139 @@
+//------------------------------------------------------------------------------
+// MJMUD Backend
+// Copyright (C) 2022, Matt Jordan
+//
+// This program is free software, distributed under the terms of the
+// MIT License. See the LICENSE file at the top of the source tree.
+//------------------------------------------------------------------------------
+
+import PlayerCharacter from '../../characters/playerCharacter.js';
+
+/**
+ * @module game/commands/default/GetItemAction
+ */
+
+/**
+ * Action that handles getting items from the world or objects
+ */
+class GetItemAction {
+
+  /**
+   * Create a new GetItemAction
+   *
+   * @param {String} target      - The item to get
+   * @param {String} [container] - Optional. The place to look for the item
+   */
+  constructor(target, container) {
+    this.target = target;
+    this.container = container;
+  }
+
+  /**
+   * Execute the action on the character
+   *
+   * @param {PlayerCharacter} character - The character to execute on
+   */
+  async execute(character) {
+    const room = character.room;
+    if (!room) {
+      character.sendImmediate('You are floating in a void.');
+      return;
+    }
+
+    let container;
+    if (this.container) {
+      const location = PlayerCharacter.physicalLocations.find(location =>
+        character.physicalLocations[location].item
+        && character.physicalLocations[location].item.name === this.container);
+      if (location) {
+        container = character.physicalLocations[location].item;
+      } else {
+        container = character.inanimates.find(item => item.name === this.container);
+        if (!container) {
+          container = room.inanimates.find(item => item.name === this.container);
+          if (!container) {
+            character.sendImmediate(`${this.container} does not exist`);
+            return;
+          }
+        }
+      }
+
+      if (!container.isContainer) {
+        character.sendImmediate(`${this.container} is not a container`);
+        return;
+      }
+    } else {
+      container = room;
+    }
+
+    const items = [];
+    if (this.target !== 'all') {
+      const item = container.inanimates.find(i => i.name === this.target);
+      if (!item) {
+        character.sendImmediate(`${this.target} is not in ${container.name}`);
+        return;
+      }
+      items.push(item);
+    } else {
+      items.push(...container.inanimates);
+    }
+
+    items.forEach((item) => {
+      if (!container.removeItem(item)) {
+        character.sendImmediate(`You cannot remove ${item.name} from ${container.name}`);
+      } else {
+        character.addHauledItem(item);
+        character.sendImmediate(`You put ${item.name} in your inventory`);
+      }
+    });
+  }
+}
+
+/**
+ * Factory for generating GetItemAction from player text
+ */
+class GetItemFactory {
+
+  /**
+   * The mapping of this factory to the player command
+   */
+  static get name() {
+    return 'Get';
+  }
+
+  /**
+   * Create a new factory
+   */
+  constructor() {
+  }
+
+  /**
+   * Generate a GetItemAction from the provided player input
+   *
+   * @param {Array.<String>} tokens - The text the player provided
+   *
+   * @return {GetItemAction} On success the action to execute, or null
+   */
+  generate(tokens) {
+    if (!tokens || tokens.length === 0) {
+      return null;
+    }
+
+    let action;
+    const index = tokens.indexOf('from');
+    if (index === -1) {
+      action = new GetItemAction(tokens.join(' '));
+    } else {
+      const target = tokens.splice(0, index);
+      tokens.splice(0, 1);
+      action = new GetItemAction(target.join(' '), tokens.join(' '));
+    }
+
+    return action;
+  }
+}
+
+export {
+  GetItemAction,
+  GetItemFactory,
+};
---
+++ test/game/commands/default/testGetItem.js
@@ -0,0 +1,249 @@
+//------------------------------------------------------------------------------
+// MJMUD Backend
+// Copyright (C) 2022, Matt Jordan
+//
+// This program is free software, distributed under the terms of the
+// MIT License. See the LICENSE file at the top of the source tree.
+//------------------------------------------------------------------------------
+
+import assert from 'power-assert';
+
+import { GetItemAction, GetItemFactory } from '../../../../src/game/commands/default/GetItem.js';
+import { FakeClient, createWorld, destroyWorld } from '../../fixtures.js';
+import { backpackFactory } from '../../../../src/game/objects/armor.js';
+import { longswordFactory } from '../../../../src/game/objects/weapons.js';
+
+describe('GetItemAction', () => {
+
+  let pc;
+
+  beforeEach(async () => {
+    const results = await createWorld();
+    pc = results.pc1;
+    pc.transport = new FakeClient();
+  });
+
+  afterEach(async () => {
+    await destroyWorld();
+  });
+
+  describe('when the item does not exist', () => {
+    it('tells the player that the item does not exist', async () => {
+      const uut = new GetItemAction('Longsword');
+      await uut.execute(pc);
+      assert(pc.transport.sentMessages.length === 1);
+      assert.match(pc.transport.sentMessages[0], /Longsword is not in/);
+    });
+  });
+
+  describe('when the container does not exist', () => {
+    beforeEach(async () => {
+      const weapon = await longswordFactory();
+      pc.room.addItem(weapon);
+    });
+
+    it('tells the player that the container does not exist', async () => {
+      const uut = new GetItemAction('Longsword', 'Backpack');
+      await uut.execute(pc);
+      assert(pc.transport.sentMessages.length === 1);
+      assert.match(pc.transport.sentMessages[0], /Backpack does not exist/);
+    });
+  });
+
+  describe('when the keyword "all" is used', () => {
+    describe('when its in a room', () => {
+      beforeEach(async () => {
+        const weapon1 = await longswordFactory();
+        const weapon2 = await longswordFactory();
+        pc.room.addItem(weapon1);
+        pc.room.addItem(weapon2);
+      });
+
+      it('the player picks everything up', async () => {
+        const uut = new GetItemAction('all');
+        await uut.execute(pc);
+        assert(pc.transport.sentMessages.length === 2);
+        assert.match(pc.transport.sentMessages[0], /You put Longsword in your inventory/);
+        assert.match(pc.transport.sentMessages[1], /You put Longsword in your inventory/);
+        assert(pc.inanimates.length === 2);
+      });
+    });
+
+    describe('when a container is specified', () => {
+      let backpack;
+
+      beforeEach(async () => {
+        const weapon1 = await longswordFactory();
+        const weapon2 = await longswordFactory();
+        backpack = await backpackFactory();
+        backpack.addItem(weapon1);
+        backpack.addItem(weapon2);
+      });
+
+      describe('when the container is in the room', () => {
+        beforeEach(() => {
+          pc.room.addItem(backpack);
+        });
+
+        it('the player picks everything up', async () => {
+          const uut = new GetItemAction('all', 'Backpack');
+          await uut.execute(pc);
+          assert(pc.transport.sentMessages.length === 2);
+          assert.match(pc.transport.sentMessages[0], /You put Longsword in your inventory/);
+          assert.match(pc.transport.sentMessages[1], /You put Longsword in your inventory/);
+          assert(pc.inanimates.length === 2);
+        });
+      });
+
+      describe('when the container is on their person', () => {
+        beforeEach(() => {
+          pc.physicalLocations.back.item = backpack;
+        });
+
+        it('the player picks everything up', async () => {
+          const uut = new GetItemAction('all', 'Backpack');
+          await uut.execute(pc);
+          assert(pc.transport.sentMessages.length === 2);
+          assert.match(pc.transport.sentMessages[0], /You put Longsword in your inventory/);
+          assert.match(pc.transport.sentMessages[1], /You put Longsword in your inventory/);
+          assert(pc.inanimates.length === 2);
+        });
+      });
+
+      describe('when the container is in their inventory', () => {
+        beforeEach(() => {
+          pc.addHauledItem(backpack);
+        });
+
+        it('the player picks everything up', async () => {
+          const uut = new GetItemAction('all', 'Backpack');
+          await uut.execute(pc);
+          assert(pc.transport.sentMessages.length === 2);
+          assert.match(pc.transport.sentMessages[0], /You put Longsword in your inventory/);
+          assert.match(pc.transport.sentMessages[1], /You put Longsword in your inventory/);
+          assert(pc.inanimates.length === 3);
+        });
+      });
+    });
+  });
+
+  describe('when a single item is specified', () => {
+    describe('when its in a room', () => {
+      beforeEach(async () => {
+        const weapon1 = await longswordFactory();
+        pc.room.addItem(weapon1);
+      });
+
+      it('the player picks it up', async () => {
+        const uut = new GetItemAction('Longsword');
+        await uut.execute(pc);
+        assert(pc.transport.sentMessages.length === 1);
+        assert.match(pc.transport.sentMessages[0], /You put Longsword in your inventory/);
+        assert(pc.inanimates.length === 1);
+      });
+    });
+
+    describe('when a container is specified', () => {
+      let backpack;
+
+      beforeEach(async () => {
+        const weapon1 = await longswordFactory();
+        backpack = await backpackFactory();
+        backpack.addItem(weapon1);
+      });
+
+      describe('when the container is in the room', () => {
+        beforeEach(() => {
+          pc.room.addItem(backpack);
+        });
+
+        it('the player picks it up', async () => {
+          const uut = new GetItemAction('Longsword', 'Backpack');
+          await uut.execute(pc);
+          assert(pc.transport.sentMessages.length === 1);
+          assert.match(pc.transport.sentMessages[0], /You put Longsword in your inventory/);
+          assert(pc.inanimates.length === 1);
+        });
+      });
+
+      describe('when the container is on their person', () => {
+        beforeEach(() => {
+          pc.physicalLocations.back.item = backpack;
+        });
+
+        it('the player picks it up', async () => {
+          const uut = new GetItemAction('Longsword', 'Backpack');
+          await uut.execute(pc);
+          assert(pc.transport.sentMessages.length === 1);
+          assert.match(pc.transport.sentMessages[0], /You put Longsword in your inventory/);
+          assert(pc.inanimates.length === 1);
+        });
+      });
+
+      describe('when the container is in their inventory', () => {
+        beforeEach(() => {
+          pc.addHauledItem(backpack);
+        });
+
+        it('the player picks it up', async () => {
+          const uut = new GetItemAction('Longsword', 'Backpack');
+          await uut.execute(pc);
+          assert(pc.transport.sentMessages.length === 1);
+          assert.match(pc.transport.sentMessages[0], /You put Longsword in your inventory/);
+          assert(pc.inanimates.length === 2);
+        });
+      });
+    });
+  });
+
+});
+
+describe('GetItemFactory', () => {
+  describe('when no item is specified', () => {
+    it('rejects the action', () => {
+      const factory = new GetItemFactory();
+      const result = factory.generate();
+      assert(result === null);
+    });
+
+    it('rejects the action', () => {
+      const factory = new GetItemFactory();
+      const result = factory.generate([]);
+      assert(result === null);
+    });
+  });
+
+  describe('when the item is not in something', () => {
+    it('generates the action with the expected target', () => {
+      const factory = new GetItemFactory();
+      const result = factory.generate(['thing']);
+      assert(result);
+      assert(result.target === 'thing');
+    });
+
+    it('handles articles and misc words', () => {
+      const factory = new GetItemFactory();
+      const result = factory.generate(['the', 'thing']);
+      assert(result);
+      assert(result.target === 'the thing');
+    });
+  });
+
+  describe('when the item is in something', () => {
+    it('generates the action with the exepcted targets', () => {
+      const factory = new GetItemFactory();
+      const result = factory.generate(['thing', 'from', 'backpack']);
+      assert(result);
+      assert(result.target === 'thing');
+      assert(result.container === 'backpack');
+    });
+
+    it('handles articles and misc words', () => {
+      const factory = new GetItemFactory();
+      const result = factory.generate(['the', 'thing', 'from', 'the', 'backpack']);
+      assert(result);
+      assert(result.target === 'the thing');
+      assert(result.container === 'the backpack');
+    });
+  });
+});
