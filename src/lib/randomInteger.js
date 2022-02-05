f03e05f3c21e095ef4d24a2960d729e73aecf94c
Matt Jordan <mjordan@digium.com>
2022-02-05 07:09:07 -0800

    randomInteger: Add a utility function for computing random numbers
    This adds a small utility function that will provide a random integer
    between a minimum and a maximum. Thanks StackOverflow yet again.

---
+++ src/lib/randomInteger.js
@@ -0,0 +1,27 @@
+//------------------------------------------------------------------------------
+// MJMUD Backend
+// Copyright (C) 2022, Matt Jordan
+//
+// This program is free software, distributed under the terms of the
+// MIT License. See the LICENSE file at the top of the source tree.
+//------------------------------------------------------------------------------
+
+/**
+ * @module lib/randomInteger
+ */
+
+/**
+ * Compute a random integer between two numbers
+ *
+ * With respect to stackoverflow
+ *
+ * @param {Number} min - The minimum
+ * @param {Number} max - The maximum
+ *
+ * @return {Number}
+ */
+function randomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+export default randomInteger;
