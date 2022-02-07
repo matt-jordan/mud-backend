5ecc25293932e134997d00f30c58b5c3a442cde9
Matt Jordan <mjordan@digium.com>
2022-02-06 18:26:05 -0800

    DiceBag: Add something that simulates a lot of dice
    This adds a 'bag of dice' that gives us a uniform distribution across
    some min/max values. Given a range of integers and some value N, you can
    pull values from the 'bag' and get a uniform distribution of values randomly
    ordered.
    
    Sort of like reaching your hand into a dice bag and pulling out the first D20,
    except that you won't get repeats until the 21st dice.

---
+++ test/lib/testDiceBag.js
@@ -0,0 +1,54 @@
+//------------------------------------------------------------------------------
+// MJMUD Backend
+// Copyright (C) 2022, Matt Jordan
+//
+// This program is free software, distributed under the terms of the
+// MIT License. See the LICENSE file at the top of the source tree.
+//------------------------------------------------------------------------------
+
+import assert from 'power-assert';
+
+import DiceBag from '../../src/lib/DiceBag.js';
+
+describe('DiceBag', () => {
+
+  it('generates the expected values for a single set', () => {
+    const bag = new DiceBag(1, 6, 1);
+    const results = [];
+    for (let i = 0; i < 6; i += 1) {
+      results.push(bag.getRoll());
+    }
+    results.sort();
+    for (let i = 0; i < 6; i += 1) {
+      assert(results[i] === i + 1);
+    }
+  });
+
+  it('generates the expected values for multiple sets', () => {
+    const bag = new DiceBag(1, 4, 4);
+    const results = [];
+    for (let i = 0; i < 16; i += 1) {
+      results.push(bag.getRoll());
+    }
+    results.sort();
+    for (let i = 0; i < 4; i += 1) {
+      for (let j = 0; j < 4; j += 1) {
+        assert(results[i * 4 + j] === i + 1);
+      }
+    }
+  });
+
+  it('generates expected sets when we exhaust the bag', () => {
+    const bag = new DiceBag(1, 8, 1);
+    const results = [];
+    for (let i = 0; i < 16; i += 1) {
+      results.push(bag.getRoll());
+    }
+    results.sort();
+    for (let i = 0; i < 8; i += 1) {
+      for (let j = 0; j < 2; j += 1) {
+        assert(results[i * 2 + j] === i + 1);
+      }
+    }
+  });
+});
