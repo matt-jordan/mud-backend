9458fa3bf99c37060f98daa53cbfdaa75e399ae1
Matt Jordan <mcjordan81@gmail.com>
2022-02-15 19:56:10 -0800

    test: Switch to using mongodb-memory-server for tests
    It does make our tests take a lot longer to run, but it should let
    us be able to test everything without any dependencies.

--- test/bootstrap.js
+++ test/bootstrap.js
@@ -6,14 +6,18 @@
 // MIT License. See the LICENSE file at the top of the source tree.
 //------------------------------------------------------------------------------
 
+import { MongoMemoryServer } from 'mongodb-memory-server';
 import { boot, shutdown } from '../src/bootstrap.js';
 
+let mongodb;
 let server;
 
 before(async function() {
   setTimeout(() => {
     console.log('Test execution timed out after 10 seconds');
   }, 10000);
+
+  mongodb = await MongoMemoryServer.create({ instance: { port: 5000 }});
 
   server = await boot();
   server.listen(0, () => {
@@ -24,6 +28,10 @@ before(async function() {
 });
 
 after(async function () {
+  if (mongodb) {
+    await mongodb.stop();
+  }
+
   if (server) {
     await shutdown();
     await server.shutdown();
