1f65e0e2c5bd83428606e0d8b900962290d400a8
Matt Jordan <mcjordan81@gmail.com>
2022-09-05 20:58:01 -0700

    Add a Close action
    CLOSE THE DOORS

---
+++ src/game/commands/default/Close.js
@@ -0,0 +1,82 @@
+//------------------------------------------------------------------------------
+// MJMUD Backend
+// Copyright (C) 2022, Matt Jordan
+//
+// This program is free software, distributed under the terms of the
+// MIT License. See the LICENSE file at the top of the source tree.
+//------------------------------------------------------------------------------
+
+/**
+ * @module game/commands/default/Close
+ */
+
+class CloseAction {
+
+  /**
+   * Create a new close action
+   *
+   * @param {String} target - The intended door to close
+   */
+  constructor(target) {
+    this.target = target;
+  }
+
+  /**
+   * Execute the action on the character
+   *
+   * @param {Character} character - The character to execute the action on
+   */
+  async execute(character) {
+    if (!character.room) {
+      character.sendImmediate('You are floating in a void.');
+      return;
+    }
+
+    const door = character.room.getDoor(this.target);
+    if (!door) {
+      character.sendImmediate(`You do not see a ${this.target} here`);
+      return;
+    }
+
+    if (!door.isOpen) {
+      character.sendImmediate(`The ${this.target} is already closed.`);
+      return;
+    }
+
+    door.isOpen = false;
+    character.sendImmediate(`You close the ${this.target}.`);
+    character.room.sendImmediate([character], `${character.toShortText()} closes the ${this.target}`);
+  }
+}
+
+class CloseFactory {
+
+  /**
+   * The unique name that maps this factory to the player's command
+   *
+   * @return {String}
+   */
+  static get name() {
+    return 'close';
+  }
+
+  /**
+   * Create a new factory
+   */
+  constructor() {
+  }
+
+  /**
+   * Generate a CloseAction
+   *
+   * @returns {CloseAction}
+   */
+  generate(tokens) {
+    return new CloseAction(tokens.join(' '));
+  }
+}
+
+export {
+  CloseAction,
+  CloseFactory,
+};
---
+++ test/game/commands/default/testClose.js
@@ -0,0 +1,116 @@
+//------------------------------------------------------------------------------
+// MJMUD Backend
+// Copyright (C) 2022, Matt Jordan
+//
+// This program is free software, distributed under the terms of the
+// MIT License. See the LICENSE file at the top of the source tree.
+//------------------------------------------------------------------------------
+
+import assert from 'power-assert';
+
+import Door from '../../../../src/game/objects/Door.js';
+import DoorModel from '../../../../src/db/models/DoorModel.js';
+import { FakeClient, createWorld, destroyWorld } from '../../fixtures.js';
+import { CloseAction, CloseFactory } from '../../../../src/game/commands/default/Close.js';
+
+
+describe('CloseAction', () => {
+
+  let pc;
+
+  beforeEach(async () => {
+    const results = await createWorld();
+    pc = results.pc1;
+    pc.transport = new FakeClient();
+  });
+
+  afterEach(async () => {
+    await destroyWorld();
+  });
+
+  describe('when the room has no doors', () => {
+    it('tells the player that there is no door to close', async () => {
+      const uut = new CloseAction('door');
+      await uut.execute(pc);
+      assert(pc.transport.sentMessages.length === 1);
+      assert.match(pc.transport.sentMessages[0], /You do not see a door here/);
+    });
+  });
+
+  describe('when a door exists', () => {
+    beforeEach(async () => {
+      const doorModel = new DoorModel();
+      doorModel.name = 'door';
+      await doorModel.save();
+
+      const door = new Door(doorModel);
+      await door.load();
+
+      pc.room.exits['north'].door = door;
+    });
+
+    afterEach(async () => {
+      await DoorModel.deleteMany();
+    });
+
+    describe('when the door is specified incorrectly', async () => {
+      it('tells the player that there is no door to close', async () => {
+        const uut = new CloseAction('new-door');
+        await uut.execute(pc);
+        assert(pc.transport.sentMessages.length === 1);
+        assert.match(pc.transport.sentMessages[0], /You do not see a new-door here/);
+      });
+    });
+
+    describe('when the direction is specified incorrectly', async () => {
+      it('tells the player that there is no door to close', async () => {
+        const uut = new CloseAction('south.door');
+        await uut.execute(pc);
+        assert(pc.transport.sentMessages.length === 1);
+        assert.match(pc.transport.sentMessages[0], /You do not see a south.door here/);
+      });
+    });
+
+    describe('when the door exists', () => {
+      describe('when the door is already closed', () => {
+        beforeEach(() => {
+          pc.room.exits['north'].door.isOpen = false;
+        });
+
+        it('tells them it is closed', async () => {
+          const uut = new CloseAction('door');
+          await uut.execute(pc);
+          assert(pc.transport.sentMessages.length === 1);
+          assert.match(pc.transport.sentMessages[0], /The door is already closed/);
+        });
+      });
+
+      describe('when the door is open', () => {
+        beforeEach(() => {
+          pc.room.exits['north'].door.isOpen = true;
+        });
+
+        it('closes the door', async () => {
+          const uut = new CloseAction('door');
+          await uut.execute(pc);
+          assert(pc.transport.sentMessages.length === 1);
+          assert.match(pc.transport.sentMessages[0], /You close the door/);
+          assert(pc.room.exits['north'].door.isOpen === false);
+        });
+      });
+    });
+  });
+});
+
+describe('CloseFactory', () => {
+
+  it('produces a close action', () => {
+    const uut = new CloseFactory();
+    assert(uut);
+    const action = uut.generate(['the', 'door']);
+    assert(action);
+    assert(action instanceof CloseAction);
+    assert(action.target === 'the door');
+  });
+
+});
