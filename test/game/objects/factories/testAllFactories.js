38e9f2ec9be9bab476beecae76418f3d97101bee
Matt Jordan <mcjordan81@gmail.com>
2022-09-14 19:50:17 -0700

    Make it easy to generate objects with a factory map
    Right now, you have to import a factory everywhere you want to use it to
    construct an object. That's annoying.
    
    This creates a centralized map of factories that can be invoked from some
    other piece of code. Because we have a lot of factories, we now have some
    generated unit tests that make sure that they can generate objects given
    a variety of input scenarios.

---
+++ test/game/objects/factories/testAllFactories.js
@@ -0,0 +1,154 @@
+//------------------------------------------------------------------------------
+// MJMUD Backend
+// Copyright (C) 2022, Matt Jordan
+//
+// This program is free software, distributed under the terms of the
+// MIT License. See the LICENSE file at the top of the source tree.
+//------------------------------------------------------------------------------
+
+import assert from 'power-assert';
+
+import objectFactories from '../../../../src/game/objects/factories/index.js';
+import ArmorModel from '../../../../src/db/models/ArmorModel.js';
+import WeaponModel from '../../../../src/db/models/WeaponModel.js';
+
+
+describe('Object factories', () => {
+
+  afterEach(async () => {
+    await ArmorModel.deleteMany();
+    await WeaponModel.deleteMany();
+  });
+
+  [
+    { name: 'backpack' },
+    { name: 'boots',
+      scenarios: [
+        { material: 'cloth' },
+        { material: 'leather' },
+        { material: 'hard leather' },
+        { material: 'copper' },
+        { material: 'iron' },
+        { material: 'steel' },
+      ],
+      validateFn: (scenario, actual) => {
+        if (scenario.material) {
+          assert(actual.name.includes(scenario.material), actual.name);
+        }
+      },
+    },
+    { name: 'breastplate',
+      scenarios: [
+        { material: 'leather' },
+        { material: 'hard leather' },
+        { material: 'copper' },
+        { material: 'iron' },
+        { material: 'steel' },
+      ],
+      validateFn: (scenario, actual) => {
+        if (scenario.material) {
+          assert(actual.name.includes(scenario.material), actual.name);
+        }
+      },
+    },
+    { name: 'cap',
+      scenarios: [
+        { material: 'cloth' },
+        { material: 'leather' },
+        { material: 'hard leather' },
+        { material: 'copper' },
+        { material: 'iron' },
+        { material: 'steel' },
+      ],
+      validateFn: (scenario, actual) => {
+        if (scenario.material) {
+          assert(actual.name.includes(scenario.material), actual.name);
+        }
+      },
+    },
+    { name: 'cloak' },
+    { name: 'gloves',
+      scenarios: [
+        { material: 'cloth' },
+        { material: 'leather' },
+        { material: 'hard leather' },
+        { material: 'copper' },
+        { material: 'iron' },
+        { material: 'steel' },
+      ],
+      validateFn: (scenario, actual) => {
+        if (scenario.material) {
+          assert(actual.name.includes(scenario.material), actual.name);
+        }
+      },
+    },
+    { name: 'leggings',
+      scenarios: [
+        { material: 'cloth' },
+        { material: 'leather' },
+        { material: 'hard leather' },
+        { material: 'copper' },
+        { material: 'iron' },
+        { material: 'steel' },
+      ],
+      validateFn: (scenario, actual) => {
+        if (scenario.material) {
+          assert(actual.name.includes(scenario.material), actual.name);
+        }
+      },
+    },
+    { name: 'longsword' },
+    { name: 'mace' },
+    { name: 'ring',
+      scenarios: [
+        {
+          name: 'foo',
+          description: 'bar',
+        },
+        {
+          modifiers: [
+            { type: 'attribute', value: 'strength', modifier: 2 },
+            { type: 'damageBonus', value: 6 }
+          ]
+        }
+      ],
+      validateFn: (scenario, actual) => {
+        if (scenario.name) {
+          assert(scenario.name === actual.name, scenario.name);
+        }
+        if (scenario.description) {
+          assert(scenario.description === actual.model.description,
+            `${scenario.description} != ${actual.model.description}`);
+        }
+        if (scenario.modifiers) {
+          assert(scenario.modifiers.length === actual.model.modifiers.length);
+        }
+      },
+    },
+    { name: 'robe' },
+    { name: 'shirt' },
+    { name: 'shortsword' }
+  ].forEach((testCase) => {
+    describe(`${testCase.name}`, () => {
+      const scenarios = [{}];
+      if (testCase.scenarios) {
+        scenarios.push(...testCase.scenarios);
+      }
+      scenarios.forEach((scenario) => {
+        describe(`creating ${testCase.name} with ${JSON.stringify(scenario)}`, () => {
+          it('creates the object', async () => {
+            const factory = objectFactories(testCase.name);
+            assert(factory);
+            const uut = await factory(scenario);
+            assert(uut);
+            if (testCase.validateFn) {
+              testCase.validateFn(scenario, uut);
+            } else {
+              assert(uut.name.includes(testCase.name), `${uut.name} != ${testCase.name}`);
+            }
+          });
+        });
+      });
+    });
+  });
+});
