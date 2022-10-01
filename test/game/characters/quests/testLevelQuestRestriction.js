d5c3c7fe54fdc5c0cdf2d252877dbbe9ebe34396
Matt Jordan <mcjordan81@gmail.com>
2022-09-30 20:53:51 -0700

    LevelQuestRestriction: Add a quest restriction based on character level
    This adds a class that is used by Quests to determine, based on character
    level, if a character can do the quest.

---
+++ test/game/characters/quests/testLevelQuestRestriction.js
@@ -0,0 +1,30 @@
+//------------------------------------------------------------------------------
+// MJMUD Backend
+// Copyright (C) 2022, Matt Jordan
+//
+// This program is free software, distributed under the terms of the
+// MIT License. See the LICENSE file at the top of the source tree.
+//------------------------------------------------------------------------------
+
+import assert from 'power-assert';
+
+import LevelQuestRestriction from '../../../../src/game/characters/quests/LevelQuestRestriction.js';
+
+describe('LevelQuestRestriction', () => {
+  describe('check', () => {
+    it('returns false when the level check fails the min level', () => {
+      const uut = new LevelQuestRestriction({ data: { minLevel: 3, maxLevel: 4 }});
+      assert(uut.check({ getLevel: () => { return 1; }}) === false);
+    });
+
+    it('returns false when the level check fails the max level', () => {
+      const uut = new LevelQuestRestriction({ data: { minLevel: 1, maxLevel: 3 }});
+      assert(uut.check({ getLevel: () => { return 4; }}) === false);
+    });
+
+    it('returns true when the level check is between the min and max', () => {
+      const uut = new LevelQuestRestriction({ data: { minLevel: 2, maxLevel: 4 }});
+      assert(uut.check({ getLevel: () => { return 3; }}) === true);
+    });
+  });
+});
