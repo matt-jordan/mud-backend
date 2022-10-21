443d046db6dcfb4ec510be197a9363cf456987c4
Matt Jordan <mcjordan81@gmail.com>
2022-10-20 20:26:56 -0700

    Party: Add the command to leave a party
    You should have the ability to leave a party when you no longer want to be
    in it. This adds that ability: players in a party who are not the leader
    can simply say 'party leave' and they'll get booted from the party.

---
+++ test/game/commands/party/testPartyLeave.js
@@ -0,0 +1,76 @@
+//------------------------------------------------------------------------------
+// MJMUD Backend
+// Copyright (C) 2022, Matt Jordan
+//
+// This program is free software, distributed under the terms of the
+// MIT License. See the LICENSE file at the top of the source tree.
+//------------------------------------------------------------------------------
+
+import assert from 'power-assert';
+
+import Party from '../../../../src/game/characters/Party.js';
+import { PartyLeave } from '../../../../src/game/commands/party/PartyLeave.js';
+import HumanNpcFactory from '../../../../src/game/characters/factories/HumanNpcFactory.js';
+import { FakeClient, createWorld, destroyWorld } from '../../fixtures.js';
+
+describe('PartyLeave', () => {
+  describe('execute', () => {
+    let pc;
+    let member;
+    let otherPerson;
+    let theWorld;
+    let pcParty;
+
+    beforeEach(async () => {
+      const { pc1, world } = await createWorld();
+      pc = pc1;
+      theWorld = world;
+      pc.transport = new FakeClient();
+
+      const factory = new HumanNpcFactory(theWorld, pc.room);
+      member = await factory.generate({ humanNpc: { name: 'member' }});
+      member.transport = new FakeClient();
+      theWorld.addCharacter(member);
+
+      otherPerson = await factory.generate({ humanNpc: { name: 'other' }});
+      otherPerson.transport = new FakeClient();
+      theWorld.addCharacter(otherPerson);
+
+      pcParty = await Party.createParty(pc);
+      pcParty.addMember(member);
+    });
+
+    afterEach(async () => {
+      await pcParty.destroy();
+      await destroyWorld();
+    });
+
+    describe('when the character is not in a party', () => {
+      it('tells them that', async () => {
+        const uut = new PartyLeave();
+        await uut.execute(otherPerson);
+        assert(otherPerson.transport.sentMessages.some(msg => msg.includes('You are not in a party.')));
+      });
+    });
+
+    describe('when the character is leading a party', () => {
+      it('tells them they cannot leave', async () => {
+        const uut = new PartyLeave();
+        await uut.execute(pc);
+        assert(pc.transport.sentMessages.some(msg => msg.includes('You cannot leave the party, you are leading it.')));
+        assert(pcParty.length === 2);
+      });
+    });
+
+    describe('when the character is a member', () => {
+      it('tells them and the leader that they have left the party', async () => {
+        const uut = new PartyLeave();
+        await uut.execute(member);
+        assert(member.transport.sentMessages.some(msg => msg.includes('You have left TestCharacter\'s party.')));
+        assert(pc.transport.sentMessages.some(msg => msg.includes('member has left your party')));
+        assert(pcParty.length === 1);
+      });
+    });
+  });
+
+});
