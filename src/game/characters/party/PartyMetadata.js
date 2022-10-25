0ba1de9fd7d293a12c3f10dd5c85e277fdc3c375
Matt Jordan <mcjordan81@gmail.com>
2022-10-25 06:31:26 -0700

    Party: Add a container for party metadata
    When you're partying and you want to know who to follow to the cab,
    it's time for metadata. This adds a container wrapper for tracking
    individual metadata that has been set for specific characters. There's
    a hook to add future metadata handlers, similar to auto-follow, and we've
    wired in auto-follow.

---
+++ src/game/characters/party/PartyMetadata.js
@@ -0,0 +1,87 @@
+//------------------------------------------------------------------------------
+// MJMUD Backend
+// Copyright (C) 2022, Matt Jordan
+//
+// This program is free software, distributed under the terms of the
+// MIT License. See the LICENSE file at the top of the source tree.
+//------------------------------------------------------------------------------
+
+import PartyMetadataError from './PartyMetadataError.js';
+import PartyMetadataAutoFollow from './PartyMetadataAutoFollow.js';
+
+/**
+ * Container for party metadata
+ */
+class PartyMetadata {
+  #metadataStrategies;
+  #metadata;
+
+  /**
+   * Create new party metadata
+   */
+  constructor() {
+
+    this.#metadata = {};
+    this.#metadataStrategies = {
+      'auto-follow': new PartyMetadataAutoFollow(),
+    };
+
+  }
+
+  /**
+   * Set party metadata for the character
+   *
+   * @param {Character} character      - The character setting the metadata
+   * @param {Object} metadata
+   * @param {String} metadata.property - The name of the property being set
+   * @param {String} metadata.value    - The string value
+   * @param {String} [metadata.target] - Optional target being set
+   *
+   * @throws {PartyMetadataError}
+   */
+  set(character, metadata) {
+    const { property, value, target = null } = metadata;
+
+    // Throw here if we don't know what the property is
+    if (!(property in this.#metadataStrategies)) {
+      throw new PartyMetadataError(`${property} is not a valid party setting.`);
+    }
+
+    if (!(character.id in this.#metadata)) {
+      this.#metadata[character.id] = {};
+    }
+    if (!(property in this.#metadata[character.id])) {
+      this.#metadata[character.id][property] = {};
+    }
+
+    const newValue = { value, target };
+    const oldValue = this.#metadata[character.id][property];
+    this.#metadataStrategies[property].validate(character, newValue, oldValue);
+    const createdValue = this.#metadataStrategies[property].create(character, newValue, oldValue);
+    this.#metadata[character.id][property] = createdValue;
+  }
+
+  /**
+   * Convert the metadata to JSON
+   *
+   * @param {Character} character - The character to get metadata for
+   *
+   * @returns {Object}
+   */
+  toJson(character) {
+    return Object.keys(this.#metadataStrategies).map(property => {
+      if (!this.#metadata[character.id] || !this.#metadata[character.id][property]) {
+        return null;
+      }
+
+      const value = this.#metadataStrategies[property].toJson(this.#metadata[character.id][property]);
+      if (value) {
+        return { property, value };
+      }
+      return null;
+    }).filter(e => e);
+  }
+
+}
+
+export default PartyMetadata;
---
+++ test/game/characters/party/testPartyMetadata.js
@@ -0,0 +1,30 @@
+//------------------------------------------------------------------------------
+// MJMUD Backend
+// Copyright (C) 2022, Matt Jordan
+//
+// This program is free software, distributed under the terms of the
+// MIT License. See the LICENSE file at the top of the source tree.
+//------------------------------------------------------------------------------
+
+import assert from 'power-assert';
+
+import PartyMetadata from '../../../../src/game/characters/party/PartyMetadata.js';
+import PartyMetadataError from '../../../../src/game/characters/party/PartyMetadataError.js';
+
+describe('PartyMetadata', () => {
+
+  describe('set', () => {
+    it('throws an exception when the property type is not known', () => {
+      const uut = new PartyMetadata();
+      let thrown = false;
+      try {
+        uut.set({}, { property: 'foo', value: 'wat' });
+      } catch (err) {
+        assert(err instanceof PartyMetadataError);
+        thrown = true;
+      }
+      assert(thrown);
+    });
+  });
+
+});
